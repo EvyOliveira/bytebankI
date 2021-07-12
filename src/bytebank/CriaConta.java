60c32df3886de68f01427ac77e64af16cc52f7d9
Evelyn Cristini Oliveira <evelyn.oliveira@sensedia.local>
2021-07-12 14:06:20 -0300

    Refactor default value variables and variable overrides

--- src/bytebank/CriaConta.java
+++ src/bytebank/CriaConta.java
@@ -16,6 +16,14 @@ public class CriaConta {
 		System.out.println("A primeira conta tem " + primeiraConta.saldo);
 		System.out.println("A segunda conta tem " + segundaConta.saldo);
 		
+		System.out.println(primeiraConta.agencia);
+		System.out.println(primeiraConta.numero);
+		
+		System.out.println(segundaConta.agencia);
+		
+		segundaConta.agencia = 146;
+		System.out.println("Agora, a segunda conta está"
+				+ "na agência: " + segundaConta.agencia);
 	}
 
 }
