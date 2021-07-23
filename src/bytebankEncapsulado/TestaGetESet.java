0abaee92750f11a353eba9a6d88d7730c7420a9e
Evelyn Cristini Oliveira <evelyn.oliveira@sensedia.local>
2021-07-23 16:38:52 -0300

    Refactor private attributes and reference getters and setters

--- src/bytebankEncapsulado/TestaGetESet.java
+++ src/bytebankEncapsulado/TestaGetESet.java
@@ -7,6 +7,23 @@ public class TestaGetESet {
 		Conta conta = new Conta();
 		conta.setNumero(1337);
 		System.out.println(conta.getNumero());
+		
+		Cliente evelyn = new Cliente();
+		// conta.titular = evelyn;
+		evelyn.setNome("evelyn oliveira");
+		
+		conta.setTitular(evelyn);
+		System.out.println(conta.getTitular().getNome());
+		
+		conta.getTitular().setProfissao("programadora");
+		
+		Cliente titularDaConta = conta.getTitular();
+		titularDaConta.setProfissao("programadora");
+		
+		System.out.println(titularDaConta);
+		System.out.println(evelyn);
+		System.out.println(conta.getTitular);
+	
 	}
 
 }
