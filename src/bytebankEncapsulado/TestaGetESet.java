426b46d30f686a3b5b02c8388001c9f3c0427980
Evelyn Cristini Oliveira <evelyn.oliveira@sensedia.local>
2021-07-23 15:57:18 -0300

    Add private attributes and use getters and setters

---
+++ src/bytebankEncapsulado/TestaGetESet.java
@@ -0,0 +1,12 @@
+package bytebankEncapsulado;
+
+public class TestaGetESet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Conta conta = new Conta();
+		conta.setNumero(1337);
+		System.out.println(conta.getNumero());
+	}
+
+}
